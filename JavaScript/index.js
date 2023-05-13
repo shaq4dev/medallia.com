a507a3df3bb45cf37396bc5f895b4a0ab961b3a8
Shaquille Blackwood <shaquille.blackwood95@gmail.com>
2023-05-12 19:17:26 -0500

    changes to files

---
+++ JavaScript/index.js
@@ -0,0 +1,12 @@
+// nav button click and display items
+
+// let hamburgerMobile = document.getElementById("hamburger-mob");
+// let menuMobile = document.getElementById("menu-mob");
+
+// hamburgerMobile.onclick = function(){
+//     menuMobile.style.display = block;
+// }
+
+// document.getElementById('hamburger-mob').onclick = function(){
+//     document.getElementById('menu-mob').style.display = "block";
+// }
